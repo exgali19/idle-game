c7ad0405c97c7a9fd5aae355854ae9dcb419ebdc
Edwin <exgali19@harrisonburg.k12.va.us>
2019-09-27 14:22:03 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,19 +3,17 @@ var score = 0; // Create a variable to store the score in
 var title = new Title('The Forest'); // Create a title element
 var button = new Button('Gather Wood', btnPress);   // Create a button element
 var hidden = new Button('Wood');
+var title = new Text('Gather fifty pieces of wood to build a hut')//Create a text element
 hidden.hide();
 var scr = new Text(score);                    // Create a text element
 
-setInterval(btnPress, 100000);   // Set up a loop
+setInterval(btnPress, 10000000);   // Set up a loop
 
 function btnPress() {
   score++;         // Increase the score
   scr.edit(score); // Update the page with the new score
-  if(score > 1) {
+  if(score > 0) {
     makebtn();
-  }
-  if(score < 5) {
-    hidden.hide(500);
   }
 }
 
